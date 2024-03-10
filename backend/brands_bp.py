90c517147cc9afdff5db344eead5b36aaf6b8760
MG <geborekmatt@gmail.com>
2024-03-10 16:20:43 -0400

    add brands dashboard crud

---
+++ backend/brands_bp.py
@@ -0,0 +1,65 @@
+from flask import Blueprint, request
+import pandas as pd
+from db import conn
+from psycopg2.extras import RealDictCursor
+brands_bp = Blueprint('brands_routes', __name__)
+
+@brands_bp.route('/api/brands', methods=['GET'])
+def get_brands():
+    try: 
+        cur = conn.cursor()
+        cur.execute("select * from brand")
+        rows = cur.fetchall()
+        colnames = [desc[0] for desc in cur.description]
+        df = pd.DataFrame(data=rows,columns =colnames )
+        cur.close()
+        brands = df.to_dict(orient='records')
+        return brands, 200
+    except Exception as e:
+        return {"error": str(e)}, 400
+    
+@brands_bp.route('/api/brands/add', methods=['POST'])
+def add_brand():
+    try: 
+        payload = request.get_json()
+        brand = payload.get('newData')
+        if brand['aliases'] is not None: 
+            brand['aliases'] = brand['aliases'].split(",")
+        else :
+            brand['aliases'] = [""]
+        cur = conn.cursor()
+        query = f"INSERT INTO brand (NAME,aliases,gated,possible_ip,disabled) VALUES ('{brand['name']}', ARRAY{brand['aliases']}, {brand['gated']}, {brand['possible_ip']}, {brand['disabled']});"
+        cur.execute(query)
+        cur.close()
+        conn.commit()
+        return {"data":"Brand Added"}, 200
+    except Exception as e:
+        print(e)
+        return {"error": str(e)}, 400
+@brands_bp.route('/api/brands/edit', methods=['POST'])
+def edit_brand():
+    try :
+        payload = request.get_json()
+        brand = payload.get('newData')
+        if type(brand['aliases']) == type(""):
+            brand['aliases'] = brand['aliases'].split(",")
+        query = f"update brand set name = '{brand['name']}', aliases = ARRAY{brand['aliases']}, gated={brand['gated']}, possible_ip={brand['possible_ip']}, disabled={brand['disabled']} where id = {brand['id']};"
+        cur = conn.cursor()
+        cur.execute(query)
+        conn.commit()
+        return {"data": "Brand Edited"}, 200
+    except Exception as e:
+        return {"error": str(e)}, 400
+
+@brands_bp.route('/api/brands/delete', methods=['POST'])
+def delete_brand():
+    try :
+        payload = request.get_json()
+        id = payload.get('id')
+        query = f"delete from brand where id = {id};"
+        cur = conn.cursor()
+        cur.execute(query)
+        conn.commit()
+        return {"data": "Brand Deleted"}, 200
+    except Exception as e:
+        return {"error": str(e)}, 400
---
+++ client/src/components/BrandsDashboard.vue
@@ -0,0 +1,277 @@
+<template>
+  <div class="container">
+    <DataTable
+      v-model:filters="filters"
+      v-model:editingRows="editingRows"
+      editMode="row"
+      filterDisplay="row"
+      dataKey="id"
+      :value="brands"
+      tableStyle="min-width: 50rem"
+      @row-edit-save="onRowEditSave"
+    >
+      <Column field="id" header="ID" sortable width="10%"></Column>
+      <Column filterField="name" field="name" header="Name" sortable filter>
+        <template #body="{ data }">
+          {{ data.name }}
+        </template>
+        <template #filter="{ filterModel, filterCallback }">
+          <InputText
+            v-model="filterModel.value"
+            type="text"
+            @input="filterCallback()"
+            class="p-column-filter"
+            placeholder=""
+          />
+        </template>
+        <template #editor="{ data, field }">
+          <InputText v-model="data[field]" />
+        </template>
+      </Column>
+      <Column
+        filterField="gated"
+        field="gated"
+        header="Gated"
+        sortable
+        filter
+        width="10%"
+      >
+        <template #body="{ data }">
+          {{ data.gated }}
+        </template>
+        <template #filter="{ filterModel, filterCallback }">
+          <InputText
+            v-model="filterModel.value"
+            type="text"
+            @input="filterCallback()"
+            class="p-column-filter"
+            placeholder=""
+          />
+        </template>
+        <template #editor="{ data, field }">
+          <div class="pi centered">
+            <InputSwitch v-model="data[field]"></InputSwitch
+            >{{ data[field] == false ? " No" : " Yes" }}
+          </div>
+        </template>
+      </Column>
+      <Column
+        filterField="possible_ip"
+        field="possible_ip"
+        header="Possible IP"
+        sortable
+        filter
+        width="10%"
+      >
+        <template #body="{ data }"> {{ data.possible_ip }}</template>
+        <template #filter="{ filterModel, filterCallback }">
+          <InputText
+            v-model="filterModel.value"
+            type="text"
+            @input="filterCallback()"
+            class="p-column-filter"
+            placeholder=""
+          />
+        </template>
+        <template #editor="{ data, field }">
+          <InputSwitch v-model="data[field]"></InputSwitch
+          >{{ data[field] == false ? " No" : " Yes" }}
+        </template>
+      </Column>
+      <Column
+        filterField="disabled"
+        field="disabled"
+        header="Disabled"
+        sortable
+        filter
+        width="10%"
+      >
+        <template #body="{ data }">
+          {{ data.disabled }}
+        </template>
+        <template #filter="{ filterModel, filterCallback }">
+          <InputText
+            v-model="filterModel.value"
+            type="text"
+            @input="filterCallback()"
+            class="p-column-filter"
+            placeholder=""
+          />
+        </template>
+        <template #editor="{ data, field }">
+          <InputSwitch v-model="data[field]"></InputSwitch
+          >{{ data[field] == false ? " No" : " Yes" }}
+        </template>
+      </Column>
+      <Column
+        filterField="aliases"
+        field="aliases"
+        header="Aliases"
+        sortable
+        filter
+      >
+        <template #body="{ data }">
+          {{ data.aliases }}
+        </template>
+        <template #filter="{ filterModel, filterCallback }">
+          <InputText
+            v-model="filterModel.value"
+            type="text"
+            @input="filterCallback()"
+            class="p-column-filter"
+            placeholder=""
+          />
+        </template>
+        <template #editor="{ data, field }">
+          <InputText v-model="data[field]" />
+        </template>
+      </Column>
+      <Column header="Actions">
+        <template #body="{ data }">
+          <Button
+            @click="deleteBrand(data.id)"
+            class="pi centered"
+            severity="danger"
+          >
+            <i class="pi" :class="'pi-trash'"></i>
+          </Button>
+        </template>
+      </Column>
+      <Column
+        :rowEditor="true"
+        style="width: 10%; min-width: 8rem"
+        bodyStyle="text-align:center"
+      >
+      </Column>
+    </DataTable>
+    <Button @click="getBrands" class="pi centered mt-2 mr-2"
+      ><i class="pi" :class="'pi-refresh'"></i
+    ></Button>
+    <Button @click="isAddBrandActive = true" class="pi centered mt-2"
+      ><i class="pi" :class="'pi-plus'"></i
+    ></Button>
+    <Dialog
+      v-model:visible="isAddBrandActive"
+      :style="{ width: '450px' }"
+      header="Add New Brand"
+      :modal="true"
+      class="p-fluid"
+    >
+      <div class="field">
+        <label for="name">Name</label>
+        <InputText
+          id="name"
+          v-model="newBrand.name"
+          required="true"
+          autofocus
+        />
+      </div>
+      <div class="field">
+        <p for="gated" class="pi mr-1">Gated</p>
+        <InputSwitch v-model="newBrand.gated"></InputSwitch
+        >{{ newBrand.gated == false ? " No" : " Yes" }}
+      </div>
+      <div class="field">
+        <p for="possible_ip" class="pi mr-1">Possible IP Complaint</p>
+        <InputSwitch v-model="newBrand.possible_ip"></InputSwitch
+        >{{ newBrand.possible_ip == false ? " No" : " Yes" }}
+      </div>
+      <div class="">
+        <p for="disabled" class="pi mr-1">Disabled</p>
+        <InputSwitch v-model="newBrand.disabled"></InputSwitch
+        >{{ newBrand.disabled == false ? " No" : " Yes" }}
+      </div>
+      <div class="field">
+        <label for="aliases">Aliases</label>
+        <InputText
+          id="aliases"
+          v-model="newBrand.aliases"
+          required="false"
+          autofocus
+        />
+      </div>
+      <Button @click="addBrand"> Save </Button>
+    </Dialog>
+  </div>
+</template>
+  
+  <script>
+import axios from "axios";
+import { FilterMatchMode } from "primevue/api";
+
+export default {
+  data() {
+    return {
+      brands: [],
+      editBrandActive: false,
+      editingRows: [],
+      isAddBrandActive: false,
+      newBrand: {
+        name: "",
+        gated: false,
+        possible_ip: false,
+        disabled: false,
+        aliases: "",
+      },
+      filters: {
+        name: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        gated: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        possible_ip: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        disabled: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        aliases: { value: null, matchMode: FilterMatchMode.CONTAINS },
+      },
+    };
+  },
+  components: {},
+  created() {
+    this.getBrands();
+  },
+  methods: {
+    getBrands: async function () {
+      const path = "/brands";
+      try {
+        const res = await axios.get(path, {});
+        this.brands = res.data;
+      } catch {
+        console.log("error: ");
+      }
+    },
+    addBrand: async function () {
+      const path = "/brands/add";
+      try {
+        const res = await axios.post(path, { newData: this.newBrand });
+        this.newBrand ==
+          {
+            name: "",
+            gated: false,
+            possible_ip: false,
+            disabled: false,
+            aliases: "",
+          };
+        this.getBrands();
+      } catch {
+        console.log("error: ", res.data.error);
+      }
+    },
+    onRowEditSave: async function (event) {
+      const path = "/brands/edit";
+      try {
+        let { newData, index } = event;
+        const res = await axios.post(path, { newData });
+        this.getBrands();
+      } catch {
+        console.log("error: ");
+      }
+    },
+    deleteBrand: async function (id) {
+      const path = "/brands/delete";
+      try {
+        const res = await axios.post(path, { id: id });
+        this.getBrands();
+      } catch {
+        console.log("error: ");
+      }
+    },
+  },
+};
+</script>
