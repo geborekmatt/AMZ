a87a4b310bf71491949be7f4a05c128935fb87bb
MG <geborekmatt@gmail.com>
2024-03-11 18:15:08 -0400

    add pool to db

--- backend/db.py
+++ backend/db.py
@@ -1,9 +1,17 @@
 import psycopg2
+from psycopg2 import pool
 import config
-conn = psycopg2.connect(database = config.db_database, 
+pool = psycopg2.pool.SimpleConnectionPool( 
+    3, 5,database = config.db_database, 
                         user = config.db_user, 
                         host= config.db_host,
                         password = config.db_password,
                         port = config.db_port)
+
+# conn = psycopg2.connect(database = config.db_database, 
+#                         user = config.db_user, 
+#                         host= config.db_host,
+#                         password = config.db_password,
+#                         port = config.db_port)
 
 
